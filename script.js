800aa1e5d080aedf3f91038c2a6321123f898a14
JacKetchum <jack.h.ketchum@gmail.com>
2024-04-23 15:36:44 -0400

    Updates

--- script.js
+++ script.js
@@ -22,31 +22,41 @@ let score = 0;
 let scoreText;
 let gameRunning = true;
 let projectiles;
+let lasers;
+let cursors;
+let lastFired = 0;
+let fireRate = 500; 
 
 function preload() {
-    this.load.image('background', 'assets/background.png'); // Make sure this path is correct
-    this.load.image('character', 'assets/SpaceShip.png'); // Make sure this path is correct
-    this.load.image('projectile', 'assets/projectile.png'); // Make sure this path is correct
+    this.load.image('background', 'https://labs.phaser.io/assets/skies/starfield.png');
+    this.load.image('character', 'SpaceShip.png');
+    this.load.image('projectileSmall', 'Asteroid.png');
+    this.load.image('laser', 'Laser.png');
 }
 
 function create() {
-    this.add.image(400, 300, 'background');
-    character = this.physics.add.sprite(400, 300, 'character');
-    character.setCollideWorldBounds(true);
-    projectiles = this.physics.add.group();
+    this.add.tileSprite(0, 0, this.sys.game.config.width, this.sys.game.config.height, 'background').setOrigin(0, 0);
 
-    this.input.on('wheel', (event) => {
-        if (event.deltaY < 0) {
-            character.setVelocityY(-160);
-        } else {
-            character.setVelocityY(160);
-        }
-    }, this);
+    character = this.physics.add.sprite(400, 300, 'character').setScale(0.1);
+    character.setCollideWorldBounds(true);
+
+    projectiles = this.physics.add.group();
+    lasers = this.physics.add.group();
+
+    cursors = this.input.keyboard.createCursorKeys();
+    let wasd = {
+        up: this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.W),
+        down: this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.S),
+        left: this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.A),
+        right: this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.D),
+        space: this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.SPACE)
+    };
+    cursors = Object.assign(cursors, wasd);
 
     scoreText = this.add.text(16, 16, 'Score: 0', { fontSize: '32px', fill: '#fff' });
 
     this.time.addEvent({
-        delay: 1000,
+        delay: 800, 
         callback: createProjectile,
         callbackScope: this,
         loop: true
@@ -55,45 +65,64 @@ function create() {
 
 function update() {
     if (!gameRunning) {
-        this.physics.pause();
         return;
     }
 
     projectiles.getChildren().forEach(projectile => {
         if (projectile.x < 0) {
-            projectile.destroy(); // Remove the projectile if it goes off screen
+            projectile.destroy();
         }
     });
+
+    character.setVelocity(0);
+
+    if (cursors.left.isDown) {
+        character.setVelocityX(-250);
+    } else if (cursors.right.isDown) {
+        character.setVelocityX(250);
+    }
+
+    if (cursors.up.isDown) {
+        character.setVelocityY(-250);
+    } else if (cursors.down.isDown) {
+        character.setVelocityY(250);
+    }
+
+    let time = this.time.now;
+    if (Phaser.Input.Keyboard.JustDown(cursors.space) && time > lastFired + fireRate) {
+        shootLaser();
+        lastFired = time;
+    }
 }
 
 function createProjectile() {
     const y = Phaser.Math.Between(0, this.sys.game.config.height);
-    let projectile = projectiles.create(800, y, 'projectile');
+    let projectile = projectiles.create(800, y, 'projectileSmall').setScale(0.3);
     projectile.setVelocityX(-Phaser.Math.Between(100, 200));
-
-    let type = Math.random();
-    if (type < 0.3) {
-        projectile.setScale(2);
-    } else if (type < 0.6) {
-        projectile.setScale(0.5);
-    } else {
-        projectile.setScale(1);
-        projectile.setData('type', 'coin');
-    }
+    projectile.setAngularVelocity(20);
 
     this.physics.add.collider(character, projectile, handleCollision, null, this);
+    this.physics.add.collider(lasers, projectile, destroyProjectile, null, this);
 }
 
 function handleCollision(character, projectile) {
-    if (projectile.getData('type') === 'coin') {
-        score += 10;
-        scoreText.setText(`Score: ${score}`);
-        projectile.destroy();
-    } else {
-        gameRunning = false;
-        alert(`Game Over! Your score was: ${score}`);
-        projectile.destroy();
-    }
+    gameRunning = false;
+    this.physics.pause();
+    character.setTint(0xff0000);
+    scoreText.setText(`Game Over! Your score was: ${score}`);
+}
+
+function shootLaser() {
+    let laser = lasers.create(character.x, character.y, 'laser');
+    laser.setVelocityX(300);
+    laser.setScale(0.25); 
+}
+
+function destroyProjectile(laser, projectile) {
+    projectile.destroy();
+    laser.destroy();
+    score += 100;
+    scoreText.setText(`Score: ${score}`);
 }
 
 new Phaser.Game(config);
