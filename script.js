bc537e8a9eb0c2e6e235c76bc97669a30e0e2aff
JacKetchum <jack.h.ketchum@gmail.com>
2024-05-02 15:12:12 -0400

    Update script.js

--- script.js
+++ script.js
@@ -29,6 +29,9 @@ let cursors;
 let lastFired = 0;
 let fireRate = 500;
 let asteroidTimer;
+let difficultyFactor = 1;
+let initialAsteroidDelay = 800;
+let initialAsteroidSpeed = 200;
 
 function preload() {
     this.load.image('background', 'https://labs.phaser.io/assets/skies/starfield.png');
@@ -54,15 +57,6 @@ function create() {
     });
 
     cursors = this.input.keyboard.createCursorKeys();
-    let wasd = {
-        up: this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.W),
-        down: this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.S),
-        left: this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.A),
-        right: this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.D),
-        space: this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.SPACE)
-    };
-    cursors = Object.assign(cursors, wasd);
-
     scoreText = this.add.text(16, 16, 'Score: 0', { fontSize: '32px', fill: '#fff' });
 
     hearts = this.add.group({
@@ -76,8 +70,12 @@ function create() {
         heart.setScale(0.25);
     });
 
+    setupAsteroidTimer();
+}
+
+function setupAsteroidTimer() {
     asteroidTimer = this.time.addEvent({
-        delay: 800,
+        delay: initialAsteroidDelay / difficultyFactor, // Use difficulty factor to adjust delay
         callback: createProjectile,
         callbackScope: this,
         loop: true
@@ -91,43 +89,18 @@ function update() {
         return;
     }
 
-    projectiles.getChildren().forEach(projectile => {
-        if (projectile.x < 0) {
-            projectile.destroy();
-        }
-    });
+    handlePlayerMovement();
+    handleShooting();
 
-    character.setVelocity(0);
-
-    if (cursors.left.isDown) {
-        character.setVelocityX(-250);
-    } else if (cursors.right.isDown) {
-        character.setVelocityX(250);
-    }
-
-    if (cursors.up.isDown) {
-        character.setVelocityY(-250);
-        character.setRotation(-0.2);
-    } else if (cursors.down.isDown) {
-        character.setVelocityY(250);
-        character.setRotation(0.2);
-    } else {
-        character.setRotation(0);
-    }
-
-    let time = this.time.now;
-    if (Phaser.Input.Keyboard.JustDown(cursors.space) && time > lastFired + fireRate) {
-        shootLaser();
-        lastFired = time;
-    }
+    // Increase difficulty over time
+    difficultyFactor += 0.001;
+    asteroidTimer.delay = Math.max(100, initialAsteroidDelay / difficultyFactor); // Ensure delay doesn't go below 100 ms
 }
 
 function createProjectile() {
     const y = Phaser.Math.Between(0, this.sys.game.config.height);
     let projectile = projectiles.create(800, y, 'projectileSmall').setScale(0.3);
-    projectile.setVelocityX(-Phaser.Math.Between(100, 200));
-    projectile.setAngularVelocity(20);
-
+    projectile.setVelocityX(-Phaser.Math.Between(100, 100 + 100 * difficultyFactor)); // Increase speed based on difficulty
     this.physics.add.collider(character, projectile, handleCollision, null, this);
     this.physics.add.collider(lasers, projectile, destroyProjectile, null, this);
 }
@@ -136,9 +109,9 @@ function handleCollision(character, projectile) {
     this.cameras.main.shake(250, 0.01);
     character.setTint(0xff0000);
 
-    this.time.delayedCall(500, function() {
-        character.clearTint(); // Only reset to no tint, removing the blue tint application
-    }, [], this);
+    this.time.delayedCall(500, () => {
+        character.clearTint();
+    });
 
     projectile.destroy();
     lives -= 1;
@@ -157,10 +130,23 @@ function updateHearts() {
     });
 }
 
+function handlePlayerMovement() {
+    character.setVelocity(0);
+    if (cursors.left.isDown) character.setVelocityX(-250);
+    else if (cursors.right.isDown) character.setVelocityX(250);
+    if (cursors.up.isDown) character.setVelocityY(-250);
+    else if (cursors.down.isDown) character.setVelocityY(250);
+    character.setRotation(0); // Reset rotation when not moving up or down
+}
+
 function shootLaser() {
-    let laser = lasers.create(character.x, character.y, 'laser');
-    laser.setVelocityX(300);
-    laser.setScale(0.25);
+    let time = this.time.now;
+    if (Phaser.Input.Keyboard.JustDown(cursors.space) && time > lastFired + fireRate) {
+        let laser = lasers.create(character.x, character.y, 'laser');
+        laser.setVelocityX(300);
+        laser.setScale(0.25);
+        lastFired = time + fireRate;
+    }
 }
 
 function destroyProjectile(laser, projectile) {
